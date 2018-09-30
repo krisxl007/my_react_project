430a6a2516b1a25b24969026b66b04735384327c
krisxl007 <krisxl1987@gmail.com>
2018-09-30 12:03:08 +1000

    Initial commit

---
+++ src/App2.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App2 extends Component {
+    render() {
+        return (
+            <div className="App2">
+              hello World!
+            </div>
+        );
+    }
+}
+
+export default App2;
