bf8e80097562a66b1d8d985be37e925dd6c171bb
kli <kris.li@unico.com.au>
2019-07-27 09:40:48 +1000

    enable redux dev tool

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '@/reducers/index'
 
@@ -6,4 +6,7 @@ const initialState = {};
 const middleware = [thunk];
 
 // create a redux store with given reducer/initialState/middleWare
-export const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
+export const store = createStore(rootReducer, initialState, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
